a857a9d35282c27aacb2ca0b326c2322bd095f87
q2913736805 <43591813+q2913736805@users.noreply.github.com>
2019-12-09 09:18:38 +0800

    Add files via upload

--- src/main/java/cn/wzvtc/soft/TestController.java
+++ src/main/java/cn/wzvtc/soft/TestController.java
@@ -1,22 +1,35 @@
 package cn.wzvtc.soft;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
 
-@CrossOrigin
-@RestController("/" )
+@CrossOrigin(value = "http://127.0.0.1:5500",allowCredentials = "true")
+@RestController("/")
 public class TestController {
 
-    @RequestMapping(value="/data.json",method={RequestMethod.GET})
-    public Map<String,String> bookById(){
+    @RequestMapping(value="/userinfo")
+    public Map<String,String> userinfo(HttpSession httpSession){
         Map<String,String> resultMap=new HashMap<>();
-        resultMap.put("myname","潘某");
-        resultMap.put("mynumber","180020900");
+        if(httpSession.getAttribute("loginnumber")!=null) {
+            resultMap.put("myname", "潘某");
+            resultMap.put("mynumber", "180020900");
+        }
         return resultMap;
     }
+
+    @RequestMapping(value="/login")
+    public Map<String,String> login(@RequestBody Map<String,String> map, HttpSession httpSession) {
+        String password=map.get("password");
+        String number=map.get("number");
+        Map<String,String> resultMap=new HashMap<>();
+        if ("180020900".equals(number) && "180020900".equals(password)) {
+            httpSession.setAttribute("loginnumber",number);
+            resultMap.put("result", "success");
+        }
+        return resultMap;
+    }
+
 }
